4b42376f18df20735b9f5d382d123e7e8074aba1
Samet Can <138519912+canimzali@users.noreply.github.com>
2024-08-08 17:01:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,37 +14,35 @@ st.title('Sigorta Fiyatı Tahmin Uygulaması')
 st.write("Sigorta veri setini yükleyin ve modeli eğitin.")
 
 
+df = pd.read_csv('https://raw.githubusercontent.com/canimzali/dp-machinelearning/master/insurance.csv')
+st.write(df.head())
 
-    df = pd.read_csv('https://raw.githubusercontent.com/canimzali/dp-machinelearning/master/insurance.csv')
-    st.write(df.head())
+# 2. Veri ön işleme
+st.write("Veri ön işleme yapılıyor...")
+le_sex = LabelEncoder()
+le_smoker = LabelEncoder()
+le_region = LabelEncoder()
+            
+df['sex'] = le_sex.fit_transform(df['sex'])
+df['smoker'] = le_smoker.fit_transform(df['smoker'])
+df['region'] = le_region.fit_transform(df['region'])
 
-    # 2. Veri ön işleme
-    st.write("Veri ön işleme yapılıyor...")
+X = df.drop('charges', axis=1)
+y = df['charges']
+
+# 3. Veriyi eğitim ve test setlerine ayırma
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    le_sex = LabelEncoder()
-    le_smoker = LabelEncoder()
-    le_region = LabelEncoder()
-    
-    df['sex'] = le_sex.fit_transform(df['sex'])
-    df['smoker'] = le_smoker.fit_transform(df['smoker'])
-    df['region'] = le_region.fit_transform(df['region'])
+# 4. Modeli eğitme
+st.write("Model eğitiliyor...")
+model=RandomForestRegressor()
+model.fit(X_train, y_train)
 
-    X = df.drop('charges', axis=1)
-    y = df['charges']
+st.write("Model başarıyla eğitildi!")
 
-    # 3. Veriyi eğitim ve test setlerine ayırma
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    # 4. Modeli eğitme
-    st.write("Model eğitiliyor...")
-    model=RandomForestRegressor()
-    model.fit(X_train, y_train)
-
-    st.write("Model başarıyla eğitildi!")
-
-    # 5. Modeli kaydetme
-    with open('trained_model.pkl', 'wb') as file:
-        pickle.dump(model, file)
+# 5. Modeli kaydetme
+with open('trained_model.pkl', 'wb') as file:
+    pickle.dump(model, file)
     st.write("Model kaydedildi.")
 
     # 6. Tahmin yapma
@@ -66,7 +64,7 @@ st.write("Sigorta veri setini yükleyin ve modeli eğitin.")
         'region': [le_region.transform([region])[0]]
     })
 
-    if st.button('Tahmin Et'):
+if st.button('Tahmin Et'):
         prediction = model.predict(input_data)[0]
         st.write(f"Tahmini Sigorta Fiyatınız: ${prediction:.2f}")
     
