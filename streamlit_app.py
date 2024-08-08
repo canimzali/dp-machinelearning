45fd807d42e03a030c6e605cf8177478eb1ed0c3
Samet Can <138519912+canimzali@users.noreply.github.com>
2024-08-08 16:33:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from xgboost import XGBRegressor
 import pickle
 
 # 1. Veri setini yükleme
