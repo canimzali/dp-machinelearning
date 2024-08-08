993d295c72a9c324c1b67fdcc987da0672d6f052
Samet Can <138519912+canimzali@users.noreply.github.com>
2024-08-08 15:43:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd 
 
 st.title('🤖 Predict of Insurance Price')
 
 st.write('This is a machine learning app')
+
+df=pd.read_csv("https://raw.githubusercontent.com/canimzali/dp-machinelearning/master/insurance.csv")
+df
